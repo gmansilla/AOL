1bd87989ede87c8103d7d9f1292e7a2763242a73
Guillermo Mansilla <mansillag@gmail.com>
2013-11-13 16:08:45 -0500

    adding support for multiple messages per phase. fixing bug where path to script was wrong

--- LadyJavaOverWorld/LadyJava/LadyJava/LadyJava/Npc.cs
+++ LadyJavaOverWorld/LadyJava/LadyJava/LadyJava/Npc.cs
@@ -17,7 +17,7 @@ namespace LadyJava
     class Npc
     {
         Sprite sprite;
-        string[,] script = new string[6, 1];
+        string[,] script = new string[6, 4];
         private Vector2 Position
         { get { return sprite.Position; } }
 
@@ -37,59 +37,69 @@ namespace LadyJava
            
             bool readingStage1, readingStage2, readingStage3, readingStage4, readingOnCompleted;
             readingStage1 = readingStage2 = readingStage3 = readingStage4 = readingOnCompleted = false; 
-            string fileLocation = Global.ContentPath + name + "\\script.txt";
+            string fileLocation = Global.ContentPath + "\\NPC\\" + name + "\\script.txt";
             try
             {
                 using (StreamReader sr = new StreamReader(fileLocation))
                 {
                     string lines = sr.ReadToEnd();
                     string[] line = lines.Split(new Char[] { '\n' });
+                    int i = 0;
                     for (int y = 0; y < line.Length; y++)
                     {
                         switch (line[y].Trim()) { 
                             case "[Stage1]":
+                                i = 0;
                                 readingStage1 = true;
                                 readingStage2 = readingStage3 = readingStage4 = readingOnCompleted = false;
                             break;
                             case "[Stage2]":
+                                i = 0;
                                 readingStage2 = true;
                                 readingStage1 = readingStage3 = readingStage4 = readingOnCompleted = false;
                             break;
                             case "[Stage3]":
+                                i = 0;
                                 readingStage3 = true;
                                 readingStage2 = readingStage1 = readingStage4 = readingOnCompleted = false;
                             break;
                             case "[Stage4]":
+                                i = 0;
                                 readingStage4 = true;
                                 readingStage2 = readingStage3 = readingStage1 = readingOnCompleted = false;
                             break;
                             case "[onCompleted]":
+                                i = 0;
                                 readingOnCompleted = true;
                                 readingStage2 = readingStage3 = readingStage1 = readingStage4 = false;
                             break;
                             default:
                                 if (readingStage1 == true) {
-                                    script[1, 0] = line[y].Trim();
-                                    readingStage1 = false;
+                                    script[1, i] = line[y].Trim();
+                                    i++;
                                 }
                                 else if (readingStage2 == true) {
-                                    script[2, 0] = line[y].Trim();
-                                    readingStage2 = false;
+                                    readingStage1 = false;
+                                    script[2, i] = line[y].Trim();
+                                    i++;
                                 }
                                 else if (readingStage3 == true)
                                 {
-                                    script[3, 0] = line[y].Trim();
-                                    readingStage3 = false;
+                                    script[3, i] = line[y].Trim();
+                                    i++;
+                                    readingStage2 = false;
                                 }
                                 else if (readingStage4 == true)
                                 {
-                                    script[4, 0] = line[y].Trim();
-                                    readingStage4 = false;
+                                    script[4, i] = line[y].Trim();
+                                    i++;
+                                    readingStage3 = false;
                                 }
                                 else if (readingOnCompleted == true)
                                 {
-                                    script[5, 0] = line[y].Trim();
-                                    readingOnCompleted = false;
+                                    script[5, i] = line[y].Trim();
+                                    i++;
+                                    //readingOnCompleted = false;
                                 }
                             break;
                         }
